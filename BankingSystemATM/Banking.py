edd3e28162aa34e3d26c150321380b533c5a1d05
Ananya Saxena <ana.saxena07@gmail.com>
2025-04-06 14:34:16 +0530

    commit

--- BankingSystemATM/Banking.py
+++ BankingSystemATM/Banking.py
@@ -1,3 +1,21 @@
 # Simulate an ATM system for checking balance, withdrawal, and deposit.
 # Use loops for multiple transactions and conditionals to prevent overdraft.
 
+class Banking:
+
+    def __init__(self):
+        self.balance =1000
+
+    def withdrawal(self, withdraw_amount):
+        if self.balance > withdraw_amount:
+            self.balance -= withdraw_amount
+            print(f"Withdrawal successful! Your new balance is: {self.balance}")
+
+        else:
+            print("Insufficient balance! Please try again.")
+
+    
+    def deposit(self, deposit_amount):
+        self.balance += deposit_amount
+        print(f"Deposit successful! Your new balance is: {self.balance}")
+
---
+++ BankingSystemATM/Main.py
@@ -0,0 +1,28 @@
+import Banking
+
+banking = Banking.Banking()
+
+while True:
+    print("Welcome to the ATM!")
+    print("1. Check Balance")
+    print("2. Withdrawal Money")
+    print("3. Deposit Money")
+    print("4. Exit")
+    user_choice = int(input("Please enter your choice: "))
+
+    if user_choice == 1:
+        print(f"Your balance is: {banking.balance}")
+
+    elif user_choice == 2:
+        withdraw_amount = int(input("Enter amount to withdraw: "))
+        banking.withdrawal(withdraw_amount)
+
+    elif user_choice == 3:
+        deposit_amount = int(input("Enter amount to be deposit: "))
+        banking.deposit(deposit_amount)
+
+    elif user_choice == 4:
+        print("Thank you for using the ATM. Goodbye!")
+        break
+    else:
+        print("Invalid choice! Please try again.")
